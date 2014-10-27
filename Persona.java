346466bf1e98992d6ce433f0f608701951889c76
Sourdaci <sourdaci@gmail.com>
2014-10-27 08:48:35 +0100

    Persona que pide y muestra edad

---
+++ Persona.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Persona here.
+ * 
+ * @author Sourdaci 
+ * @version 2014/10/24/01
+ */
+public class Persona
+{
+    // instance variables - replace the example below with your own
+    private int edad;
+    
+    /**
+     * Constructor for objects of class Persona
+     */
+    public Persona(int leerEdad)
+    {
+        // initialise instance variables
+        edad = leerEdad;
+    }
+
+    /**
+     * Se supone que aqui va el método que devuelve la edad de la persona
+     */
+    public int getEdad()
+    {
+        // put your code here
+        return edad;
+    }
+}
